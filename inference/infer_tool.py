bd86829bc10063013567075948e1eebcb3444733
dvrk-dvys <ja.harr91@gmail.com>
2023-08-21 14:02:07 +0200

    colab handle

--- inference/infer_tool.py
+++ inference/infer_tool.py
@@ -187,12 +187,12 @@ class Svc(object):
         if self.nsf_hifigan_enhance:
             from model_dir.modules.enhancer import Enhancer
             if utils.is_running_in_colab() == True:
-                zip_path = 'drive/MyDrive/dataset/44k/44k/nsf_hifigan_20221211.zip'
+                zip_path = 'content/drive/MyDrive/dataset/44k/44k/nsf_hifigan_20221211.zip'
                 urllib.request.urlretrieve(
                     'https://github.com/openvpi/vocoders/releases/download/nsf-hifigan-v1/nsf_hifigan_20221211.zip',
                     zip_path)
                 # Unzipping the model
-                enhancer_model_path = 'drive/MyDrive/dataset/44k/44k/nsf_hifigan/'
+                enhancer_model_path = 'content/drive/MyDrive/dataset/44k/44k/nsf_hifigan/'
 
                 with zipfile.ZipFile(zip_path, 'r') as zip_ref:
                     zip_ref.extractall(enhancer_model_path)
