174a2b3b3ec3b805f875694f07c4e36c6fc498c3
dvrk-dvys <ja.harr91@gmail.com>
2023-12-14 01:25:43 +0100

    48000 sr

--- preprocess/resample.py
+++ preprocess/resample.py
@@ -7,7 +7,7 @@ file = '/Users/jordanharris/Code/PycharmProjects/EZ_RVC/input/SZA_CTRL/1_SZA - S
 y_original, sr_original = librosa.load(file, sr=None)
 
 # Load the audio file with resampling
-target_sample_rate = 22050  # Replace with your desired sample rate
+target_sample_rate = 44100  # Replace with your desired sample rate
 y_resampled, sr_resampled = librosa.load(file, sr=target_sample_rate)
 
 # Plot the original audio
